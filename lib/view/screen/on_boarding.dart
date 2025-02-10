fb06221810dfa37b157b57e450b6ee7c86ec405a
saad <saadworkdotcom@gmail.com>
2025-02-10 22:13:08 +0000

    add Cairo font assets and onboarding screen

---
+++ lib/view/screen/on_boarding.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('OnBoarding'),
+      ),
+      body: const Center(
+        child: Text('OnBoarding'),
+      ),
+    );
+  }
+}
